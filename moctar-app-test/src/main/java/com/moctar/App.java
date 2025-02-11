06dd1f892d0bd2f3ede8b7b12ef0fab05eb83806
foundikou <67112557+foundikou@users.noreply.github.com>
2025-02-11 16:56:17 +0100

    Add files via upload

---
+++ moctar-app-test/src/main/java/com/moctar/App.java
@@ -0,0 +1,13 @@
+package com.moctar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
